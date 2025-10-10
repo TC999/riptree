2d6d732a21bae3511620846211f5662d55be74ca
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-10-10 13:13:46 +0800

    代码模块化

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,8 @@
 // 是否显示隐藏文件
 static mut SHOW_HIDDEN: bool = false;
-use std::fs;
-use std::path::Path;
 
 mod help;
-
-
-fn print_tree(path: &Path, prefix: String) {
-    if let Ok(entries) = fs::read_dir(path) {
-        let mut entries: Vec<_> = entries.filter_map(Result::ok).collect();
-        // 过滤隐藏文件
-        unsafe {
-            if !SHOW_HIDDEN {
-                entries = entries
-                    .into_iter()
-                    .filter(|e| {
-                        if let Some(name) = e.file_name().to_str() {
-                            !name.starts_with('.')
-                        } else {
-                            true
-                        }
-                    })
-                    .collect();
-            }
-        }
-        entries.sort_by_key(|e| e.path());
-
-        let len = entries.len();
-        for (i, entry) in entries.into_iter().enumerate() {
-            let file_name = entry.file_name().into_string().unwrap_or_default();
-            let is_last = i == len - 1;
-            let connector = if is_last { "└── " } else { "├── " };
-            println!("{}{}{}", prefix, connector, file_name);
-
-            let new_prefix = if is_last {
-                format!("{}    ", prefix)
-            } else {
-                format!("{}│   ", prefix)
-            };
-
-            if entry.path().is_dir() {
-                print_tree(&entry.path(), new_prefix);
-            }
-        }
-    }
-}
+mod print;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -68,5 +26,5 @@ fn main() {
     let path = path.unwrap_or_else(|| ".".to_string());
     let root = std::path::Path::new(&path);
     println!("{}", root.display());
-    print_tree(root, String::new());
+    print::print_tree(root, String::new());
 }
---
+++ src/print.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::path::Path;
+use crate::SHOW_HIDDEN;
+
+pub fn print_tree(path: &Path, prefix: String) {
+    if let Ok(entries) = fs::read_dir(path) {
+        let mut entries: Vec<_> = entries.filter_map(Result::ok).collect();
+        // 过滤隐藏文件
+        unsafe {
+            if !SHOW_HIDDEN {
+                entries = entries
+                    .into_iter()
+                    .filter(|e| {
+                        if let Some(name) = e.file_name().to_str() {
+                            !name.starts_with('.')
+                        } else {
+                            true
+                        }
+                    })
+                    .collect();
+            }
+        }
+        entries.sort_by_key(|e| e.path());
+
+        let len = entries.len();
+        for (i, entry) in entries.into_iter().enumerate() {
+            let file_name = entry.file_name().into_string().unwrap_or_default();
+            let is_last = i == len - 1;
+            let connector = if is_last { "└── " } else { "├── " };
+            println!("{}{}{}", prefix, connector, file_name);
+
+            let new_prefix = if is_last {
+                format!("{}    ", prefix)
+            } else {
+                format!("{}│   ", prefix)
+            };
+
+            if entry.path().is_dir() {
+                print_tree(&entry.path(), new_prefix);
+            }
+        }
+    }
+}
