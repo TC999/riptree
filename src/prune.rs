4baacf30a0632c14be5381ec975195bd2b9dd30c
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-10-10 14:16:15 +0800

    移除部分警告

--- src/prune.rs
+++ src/prune.rs
@@ -1,6 +1,6 @@
 pub fn is_dir_pruned(path: &std::path::Path) -> bool {
     match std::fs::read_dir(path) {
-        Ok(mut entries) => {
+        Ok(entries) => {
             for entry in entries.filter_map(Result::ok) {
                 let file_type = entry.file_type();
                 if let Ok(ft) = file_type {
